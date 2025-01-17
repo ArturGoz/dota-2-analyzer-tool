3d46665b96fe2a4a2c0344ddcaa113eea6b0c2bd
ArturGoz <arturrm4@gmail.com>
2025-01-17 16:50:50 +0200

    trying to change logic of security to jwt

---
+++ auth-service/src/test/java/artur/goz/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package artur.goz.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
